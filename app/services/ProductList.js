86534aaa19af55b3383b31c5985512452de07b83
Khanh <khanhquangdinh197@gmail.com>
2022-06-17 07:40:32 +0700

    update code

--- app/services/ProductList.js
+++ app/services/ProductList.js
@@ -3,7 +3,7 @@ function ProductList (){
     this.getList = function(){
         return axios({
             method : 'get',
-            url : 'https://62a8007ca89585c1770aba5e.mockapi.io/phone-list'
+            url : 'https://62a8007ca89585c1770aba5e.mockapi.io/watch-list'
         })
     }
 }
